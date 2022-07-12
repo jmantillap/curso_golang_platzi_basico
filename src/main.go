b1c19c02b964f7dda12c8f183d607047e8ceb2af
Javier Orlando Mantilla Portilla <jmantilla@intergrupo.com>
2022-07-12 16:04:54 -0500

    paqeute fmt

--- src/main.go
+++ src/main.go
@@ -87,4 +87,28 @@ func main() {
 	diametroCuadrado := math.Pow(float64(diametro), 2)
 	areaCirculo := (pi3 * diametroCuadrado) / 4
 	fmt.Println("Área de un circulo:", math.Round(areaCirculo*100)/100)
+
+	//declaracion variables paquete fmt
+	helloMessage := "Hello"
+	worldMessage := "world"
+
+	// Println: Salto de Linea Automatico
+	fmt.Println(helloMessage, worldMessage)
+	fmt.Println(helloMessage, worldMessage)
+
+	// Printf
+	nombre := "Platzi"
+	cursos := 500
+	// Con valores seguros
+	fmt.Printf("%s tiene más de %d cursos\n", nombre, cursos)
+	// Con valores inseguros
+	fmt.Printf("%v tiene más de %v cursos\n", nombre, cursos)
+
+	// Sprintf
+	message := fmt.Sprintf("%v tiene más de %v cursos\n", nombre, cursos)
+	fmt.Println(message)
+
+	// Tipo de datos:
+	fmt.Printf("helloMessage: %T\n", helloMessage)
+	fmt.Printf("cursos: %T\n", cursos)
 }
